59d7b5a209aead2bd03ceabcbe85502b0124af06
Bruno <brcossta@users.noreply.github.com>
2020-08-10 01:38:49 -0300

    Database da aplicação

--- src/server.js
+++ src/server.js
@@ -1,72 +1,8 @@
-const proffys = [
-    {
-        name: '',
-        avatar: '',
-        whatsapp: '',
-        bio: '',
-        subject: '',
-        cost: '',
-        weekday: [],
-        time_from: [],
-        time_to: [],
-    }
-]
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação Física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química",
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-Feira",
-    "Terça-Feira",
-    "Quarta-Feira",
-    "Quinta-Feira",
-    "Sexta-Feira",
-    "Sábado",
-]
-
-function getSubject(subjectNumber) {
-    const position = +subjectNumber - 1
-    return subjects[position]
-}
-
-function pageLanding ( req, res) {
-    return res.render("index.html")
-}
-
-function pageStudy (req, res) {
-    const filters = req.query
-    return res.render("study.html", { proffys, filters, subjects, weekdays })
-}
-
-function pageGiveClasses (req, res) {
-    const data = req.query
-
-    const isNotEmpty = Object.keys(data).length > 0
-    if (isNotEmpty) {
-
-        data.subject = getSubject(data.subject)
-
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-
-    return res.render("give-classes.html", { subjects, weekdays })
-}
-
 const express = require('express')
 const server = express()
+
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
+
 const nunjucks = require('nunjucks')
 
 nunjucks.configure('src/views', {
@@ -75,8 +11,9 @@ nunjucks.configure('src/views', {
 })
 
 server.use(express.static('public'))
-
+.use(express.urlencoded({ extended: true }))
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post('/save-classes', saveClasses)
 .listen(5500)
